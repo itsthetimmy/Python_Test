472eb71ec163aa55faa8f9a78811808084bdd836
itsthetimmy <t.scheuermann2000@gmail.com>
2020-07-27 01:09:29 +0200

    Funktion getpreis() hinzugefügt, läuft mit Dollar und Euro von Ard Börse

---
+++ aktie.py
@@ -0,0 +1,33 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+#Nur für ARD Börsenseite!
+#url = "https://kurse.boerse.ard.de/ard/kurse_einzelkurs_uebersicht.htn?i=9408435" # Tesla
+url = "https://kurse.boerse.ard.de/ard/fonds_einzelkurs_uebersicht.htn?i=163575" #UniRak
+
+
+def getpreis(url):
+    page=requests.get(url)
+
+    soup= BeautifulSoup(page.text, 'html.parser')
+    preis=str(soup.find(class_='leftfloat big'))
+
+    preis=preis.replace("<span class=\"leftfloat big\" title=\"aktueller Wert\">" , "")
+
+    #Währung herausfinden (geht nur Dollar und Euro)
+    if (preis.find('€') != -1):
+        waehrung = "€"
+        preis=preis.replace("€</span>" , "")#Euro etc entfernen
+        #preis=float(preis[1:])
+    elif (preis.find('$') != -1):
+        waehrung = "$"
+        preis=preis.replace(".","") #Beim Dollar den Tausender-Punkt entfernen
+        preis=preis.replace("$</span>" , "")#Dollar etc entfernen
+
+    preis=preis.replace("\xa0" , "")
+    preis=preis.replace("," , ".")
+
+    print(preis+waehrung)
+
+getpreis("https://kurse.boerse.ard.de/ard/kurse_einzelkurs_uebersicht.htn?i=107741")
